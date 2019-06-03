3bb53fe6263eaac535470392055503aac841c76a
Jairo <jairocr0410@gmail.com>
2019-06-03 16:53:58 -0600

    Vista corronga de inicio

--- src/view/home.php
+++ src/view/home.php
@@ -1,38 +1,10 @@
-<?php 
-  include_once 'header.php';
-?>
+<?= view('header.php') ?>
 
-<div class="row">
-  <?php 
-    include_once 'map.php';
-  ?>
+<h3 class="text-center">Sitio de Recomendaciones de ofertas y promociones turísticas</h3>
+<p class="text-center">Le damos la bienvenida al sitio, aquí podrá buscar ofertas y promociones de acuerdo a sus gustos.</p>
 
-  <div class="col-lg-4" style="margin: 25px; border-style: dashed; display: flow-root;">
-    <label style="margin-top: 10px;">Preferencia</label>
-    <select class="form-control">
-      <option>Sin especificar</option>
-      <option>Aventurero</option>
-      <option>Gastronomico</option>
-      <option>Historico</option>
-    </select>
-    <label style="margin-top: 10px;">Desea espacio de fumado?</label>
-    <form action="">
-      <input type="radio" name="A" value="1" checked> Si<br>
-      <input type="radio" name="B" value="0"> No<br>
-    </form>
-    <label style="margin-top: 10px;">Requiere accesso para discapacitados?</label>
-    <form action="">
-      <input type="radio" name="C" value="1" checked> Si<br>
-      <input type="radio" name="D" value="0"> No<br>
-    </form>
-    <label style="margin-top: 10px;">Lleva consigo una mascota?</label>
-    <form action="">
-      <input type="radio" name="E" value="1" checked> Si<br>
-      <input type="radio" name="F" value="0"> No<br>
-    </form>
-  <div>
+<div class="text-center">
+	<img src="img/turismo.jpg" width="75%" />
 </div>
 
-<?php 
-  include_once 'footer.php';
-?>
+<?= view('footer.php') ?>
